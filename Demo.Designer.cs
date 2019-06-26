f67fe6e612bda3d67a4b5eeeff3f3ad357a89ef5
jgsistem <jgsistem@hotmail.com>
2019-06-26 01:50:52 -0500

    Async_Await_Task

---
+++ Demo.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Demo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnoperacion = new System.Windows.Forms.Button();
+            this.serviceController1 = new System.ServiceProcess.ServiceController();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textresult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 104);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mensaje";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnoperacion
+            // 
+            this.btnoperacion.Location = new System.Drawing.Point(194, 148);
+            this.btnoperacion.Name = "btnoperacion";
+            this.btnoperacion.Size = new System.Drawing.Size(85, 48);
+            this.btnoperacion.TabIndex = 1;
+            this.btnoperacion.Text = "Operacion";
+            this.btnoperacion.UseVisualStyleBackColor = true;
+            this.btnoperacion.Click += new System.EventHandler(this.btnoperacion_Click);
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(280, 30);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(100, 20);
+            this.text1.TabIndex = 2;
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(280, 76);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(100, 20);
+            this.text2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Numero 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(191, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Numero 2";
+            // 
+            // textresult
+            // 
+            this.textresult.Location = new System.Drawing.Point(280, 113);
+            this.textresult.Name = "textresult";
+            this.textresult.Size = new System.Drawing.Size(100, 20);
+            this.textresult.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Result";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(305, 148);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(86, 48);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(50, 208);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(35, 13);
+            this.lbl1.TabIndex = 10;
+            this.lbl1.Text = "label4";
+            // 
+            // Demo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 281);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textresult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text1);
+            this.Controls.Add(this.btnoperacion);
+            this.Controls.Add(this.button1);
+            this.Name = "Demo";
+            this.Text = "Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnoperacion;
+        private System.ServiceProcess.ServiceController serviceController1;
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textresult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ Demo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Demo : Form
+    {
+        public Demo()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }       
+        //Despues
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //async y await esperan el resultado del otro metodo
+            int i = await calculoDespues();
+
+            lbl1.Text = "Numero es: " + i;
+        }
+
+        //Antes
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    int i = calculoDespues();
+
+        //    lbl1.Text = "Numero es: " + i;
+        //}
+
+        private static async Task<Int32> calculoDespues() 
+        {
+            Int32 resul =0;
+            //await y async siempre van junatos para sincronizar
+            // y tiene que devolverse con formato Task
+            await Task.Run(() =>
+            {
+                Thread.Sleep(10000);
+                for (int i = 0; i < 100; i++)
+                {
+                    resul = i;
+                }
+            });            
+
+            return resul;
+        }
+
+        private static  Int32 calculoAntes()
+        {
+            Int32 resul = 0;            
+                Thread.Sleep(10000);
+                for (int i = 0; i < 100; i++)
+                {
+                    resul = i;
+                } 
+            return resul;
+        }
+
+        private void btnoperacion_Click(object sender, EventArgs e)
+        {
+            Int32 a = Convert.ToInt32(text1.Text);
+            Int32 b = Convert.ToInt32(text2.Text);
+            Int32 C = a + b;
+            textresult.Text = C.ToString();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            text1.Text = "";
+            text2.Text = "";
+            textresult.Text = "";
+            lbl1.Text = "";
+        }
+    }
+}
